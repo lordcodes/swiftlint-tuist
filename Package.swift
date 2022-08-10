376382518d54b184787a669bce0407ea2c0199bc
Andrew Lord <andrew@lordcodes.com>
2022-08-10 23:49:53 +0100

    Update SwiftLint to 0.48.0

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/realm/SwiftLint", revision: "e497f1f"),
+        .package(url: "https://github.com/realm/SwiftLint", revision: "22fb9eb"), // v0.48.0
         .package(url: "https://github.com/apple/swift-argument-parser", from: "1.0.0"),
         .package(url: "https://github.com/scottrhoyt/SwiftyTextTable", from: "0.9.0"),
     ],
